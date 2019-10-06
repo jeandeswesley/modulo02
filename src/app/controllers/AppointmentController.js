90545eb01bc0932f01419793ff1d48ae09e8c049
jeandeswesley <jeandeswesley@hotmail.com>
2019-10-06 19:35:08 -0300

    modulo02 Implementado listando agendamento do usuario

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,26 @@
 import * as Yup from "yup";
+import { startOfHour, parseISO, isBefore } from "date-fns";
 import Appointment from "../models/Appointment";
 import User from "../models/User";
 
 class AppointmentContrloller {
+  async index(req, res) {
+    const appointments = await Appointment.findAll({
+      where: { user_id: req.userId, canceled_at: null },
+      order: ["date"],
+      attributes: ["id", "date"],
+      include: [
+        {
+          model: User,
+          as: "provider",
+          attributes: ["id", "name"]
+        }
+      ]
+    });
+
+    return res.json(appointments);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
@@ -25,6 +43,32 @@ class AppointmentContrloller {
       return res
         .status(401)
         .json({ error: "You can only create appointments with providers" });
+    }
+    /**
+     *  Check for past dates
+     */
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: "Paste dates are not permited" });
+    }
+
+    /**
+     *  Check date availability
+     */
+
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart
+      }
+    });
+
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: "Appointment date is not available" });
     }
 
     const appointment = await Appointment.create({
