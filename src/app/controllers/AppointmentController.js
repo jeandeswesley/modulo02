71c3cab0c3ae87999aad6b200325f879794272cf
jeandeswesley <jeandeswesley@hotmail.com>
2019-10-08 21:30:50 -0300

    modulo02 Implementado marcar notificaçoes como lida e cancelamento de agendamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,5 +1,5 @@
 import * as Yup from "yup";
-import { startOfHour, parseISO, isBefore, format } from "date-fns";
+import { startOfHour, parseISO, isBefore, format, subHours } from "date-fns";
 import pt from "date-fns/locale/pt";
 import File from "../models/File";
 import User from "../models/User";
@@ -63,7 +63,7 @@ class AppointmentContrloller {
       });
     } */
 
-/**
+    /**
      * Check if provider_id is a provider
      */
     const isProvider = await User.findOne({
@@ -122,6 +122,30 @@ class AppointmentContrloller {
       content: `Novo agendamento de ${user.name} para ${formattedate}`,
       user: provider_id
     });
+
+    return res.json(appointment);
+  }
+
+  async delete(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id);
+
+    if (appointment.user_id !== req.userId) {
+      return res.status(401).json({
+        error: "You don't have permission to cancel this appointment"
+      });
+    }
+
+    const dateWithSub = subHours(appointment.date, 2);
+
+    if (isBefore(dateWithSub, new Date())) {
+      return res.status(401).json({
+        error: "You can only cancel appointment cancel 2 hours in advanced."
+      });
+    }
+
+    appointment.canceled_at = new Date();
+
+    await appointment.save();
 
     return res.json(appointment);
   }
