db87efd6b2e501a7cc0908ded8ddba48fb350dbf
jeandeswesley <jeandeswesley@hotmail.com>
2019-10-16 20:38:39 -0300

    modulo02 Implementado configuraçao de fila com redis

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,7 +6,8 @@ import User from "../models/User";
 import Appointment from "../models/Appointment";
 import Notification from "../schemas/Notification";
 
-import Mail from "../../lib/Mail";
+import CancellationMail from "../jobs/CancellationMail";
+import Queue from "../../lib/Queue";
 
 class AppointmentContrloller {
   async index(req, res) {
@@ -164,17 +165,8 @@ class AppointmentContrloller {
 
     await appointment.save();
 
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: "Agendamento Cancelado",
-      template: "cancellation",
-      context: {
-        provider: appointment.provider.name,
-        user: appointment.user.name,
-        date: format(appointment.date, "'dia' dd 'de' MMM' , às' H:mm'h'", {
-          locale: pt
-        })
-      }
+    await Queue.add(CancellationMail.key, {
+      appointment
     });
 
     return res.json(appointment);
